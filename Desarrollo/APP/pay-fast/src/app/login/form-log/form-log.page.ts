228c52bc4441af10f5c20b2849fef855aeb7d163
panceta53 <47751075+panceta53@users.noreply.github.com>
2020-05-05 10:17:51 -0500

    cambios login

--- Desarrollo/APP/pay-fast/src/app/login/form-log/form-log.page.ts
+++ Desarrollo/APP/pay-fast/src/app/login/form-log/form-log.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsuarioService } from '../../Servicios/usuario.service'
+import md5 from 'md5'
 
 @Component({
   selector: 'app-form-log',
@@ -7,18 +9,60 @@ import { Router } from '@angular/router';
   styleUrls: ['./form-log.page.scss'],
 })
 export class FormLogPage implements OnInit {
+  
+  private correo: string;
+  private contrasena: string;
 
   constructor(
-    private router: Router
-  ) {
+    private router: Router,
+    private usuarioservice: UsuarioService
+  )
+  {
 
-   }
+  }
+
+  ngOnInit() {
+  }
+
+  private PromesaUsuValidar(datos : any){
+    return new Promise((resolve,reject)=>{
+      this.usuarioservice.validarUsuario(datos).subscribe((result:any)=>{
+        resolve({
+          result,resultado:'ok'
+        })
+      },(error:object)=>{
+        reject({
+          error,resultado:'error'
+        })
+      });
+    })
+  }
+
+  
+  private async ValidarUsu(){
+    const conencrip = md5(this.contrasena) 
+    const datosUsuario = {
+      us_c : this.correo, 
+      us_ca : conencrip
+    }
+
+    let result: any = null;
+    try{
+      result = await this.PromesaUsuValidar(datosUsuario)
+    }catch(error){
+      console.log(error);
+    }
+
+    console.log(result);
+
+  }
 
   private registrarse(){
     this.router.navigate(['registro/r1'])
   }
 
-  ngOnInit() {
-  }
+
+
+  
 
 }
