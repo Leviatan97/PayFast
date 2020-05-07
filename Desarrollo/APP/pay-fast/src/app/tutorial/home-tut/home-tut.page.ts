f894a44e7c09a0ba88b61c7f8154dc23f49d9eb7
EXTERMINADOR1999 <47929954+EXTERMINADOR1999@users.noreply.github.com>
2020-05-07 16:12:25 -0500

    RcompletoSlider

--- Desarrollo/APP/pay-fast/src/app/tutorial/home-tut/home-tut.page.ts
+++ Desarrollo/APP/pay-fast/src/app/tutorial/home-tut/home-tut.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-home-tut',
@@ -7,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeTutPage implements OnInit {
 
+  slides =[
+    {
+      img: '../../../assets/img-slider/ubicacion.svg',
+      titulo:'hola 1',
+      txtbtn: 'none'
+    },
+    {
+      img: '../../../assets/img-slider/cbarras.svg',
+      titulo:'hola 2',
+      txtbtn: 'none'
+    },
+    {
+      img: '../../../assets/img-slider/qr.svg',
+      titulo:'hola 3',
+      txtbtn: 'none'
+    },
+    {
+      img: '../../assets/img-slider/bolsa.svg',
+      titulo:'hola 4',
+      txtbtn: 'hidden'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
