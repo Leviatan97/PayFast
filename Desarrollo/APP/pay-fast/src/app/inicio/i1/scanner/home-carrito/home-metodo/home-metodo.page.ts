556a596d0e7a11a99e0d4a197afd09d0adb50f05
EXTERMINADOR1999 <47929954+EXTERMINADOR1999@users.noreply.github.com>
2020-06-09 10:57:20 -0500

    metodoPago

--- Desarrollo/APP/pay-fast/src/app/inicio/i1/scanner/home-carrito/home-metodo/home-metodo.page.ts
+++ Desarrollo/APP/pay-fast/src/app/inicio/i1/scanner/home-carrito/home-metodo/home-metodo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isContext } from 'vm';
 
 @Component({
   selector: 'app-home-metodo',
@@ -10,6 +11,31 @@ export class HomeMetodoPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private metodoPago:any=[];
+
+  private mPago(){
+    this.metodoPago=[
+      {
+        nombre: 'Alejandro',
+        apellido: 'Gomez',
+        tipo:'tarjetac',
+        ntarjeta: '4444555533332222',
+        vencimiento: 11/27,
+        cvv: '346',
+        icon: ''
+      },
+      {
+        nombre: 'Juan',
+        apellido: 'Perez',
+        tipo:'billeterav',
+        ntarjeta: '',
+        vencimiento:'',
+        cvv: '',
+        icon: '' 
+      }
+    ]
   }
 
 }
