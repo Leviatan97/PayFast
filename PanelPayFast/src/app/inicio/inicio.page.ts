ff1326676a27692cd96f02a91dd60cd27633b909
EXTERMINADOR1999 <47929954+EXTERMINADOR1999@users.noreply.github.com>
2020-07-01 11:00:49 -0500

    Pages

--- PanelPayFast/src/app/inicio/inicio.page.ts
+++ PanelPayFast/src/app/inicio/inicio.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  genders: Array<string>;
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
--- PanelPayFast/src/app/inicio/rsupermercado/rsupermercado.page.ts
+++ PanelPayFast/src/app/inicio/rsupermercado/rsupermercado.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-rsupermercado',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RsupermercadoPage implements OnInit {
 
-  constructor() { }
+  private nombre: string;
+  private responsable: string;
+  private correo: string;
+  private contrasena: string;
+  private contrasena2: string;
+
+
+  constructor(private toastController:ToastController) { }
 
   ngOnInit() {
+  }
+
+  private async Rsupermercado(){
+    try {
+      if(this.nombre != undefined && this.nombre != "" && this.responsable != undefined && this.responsable != "" && this.correo != undefined && this.correo != "" && this.contrasena != undefined && this.contrasena != "" && this.contrasena2 != undefined && this.contrasena2 != ""){
+        if(this.contrasena == this.contrasena2){
+          const rsupermercado={
+            nombre:this.nombre,
+            responsable:this.responsable,
+            correo:this.correo,
+            contrasena:this.contrasena
+          }
+          console.log(rsupermercado)
+        }
+        else{
+          this.presentToastC();
+        }
+      }else{
+        this.presentToast();
+      }
+      
+    } catch (error) {
+      
+  }
+}
+
+async presentToast() 
+  {
+    const toast = await this.toastController.create({
+      message: 'Asegurese de llenar los campos requeridos.',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentToastC() 
+  {
+    const toast = await this.toastController.create({
+      message: 'Las contraseña no coinciden',
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
