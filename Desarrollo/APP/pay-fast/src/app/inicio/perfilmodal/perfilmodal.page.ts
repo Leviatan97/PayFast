4d177a5dcb544b848b694164b5b27fa6f200f7a5
Sebastian Ortegon <sebastom209@gmail.com>
2020-07-09 15:54:58 -0500

    perfilmodal

--- Desarrollo/APP/pay-fast/src/app/inicio/perfilmodal/perfilmodal.page.ts
+++ Desarrollo/APP/pay-fast/src/app/inicio/perfilmodal/perfilmodal.page.ts
@@ -28,21 +28,21 @@ export class PerfilmodalPage implements OnInit {
 
   createFormGroup(){
     return new FormGroup({
-      nombre: new FormControl('',[Validators.required, Validators.minLength(50)]),
-      apellido: new FormControl('',[Validators.required, Validators.minLength(50)]),
-      fnacimiento: new FormControl('',[Validators.required]),
+      nombre: new FormControl('',[Validators.required, Validators.maxLength(50),]),
+      apellido: new FormControl('',[Validators.required, Validators.maxLength(50)]),
+      fnacimiento: new FormControl('',[Validators.required,]),
       tdocumento: new FormControl('',[Validators.required]),
-      ndocumento: new FormControl('',[Validators.required, Validators.minLength(10)]),
-      email: new FormControl('',[Validators.required, Validators.minLength(8), Validators.pattern(this.emailPattern)]),
+      ndocumento: new FormControl('',[Validators.required, Validators.min(9999999), Validators.max(9999999999)]),
+      email: new FormControl('',[Validators.required, Validators.maxLength(50), Validators.pattern(this.emailPattern)]),
     })
   }
 
-  get nombre() { return this.contactForm.get('usuario.us_n'); }
-  get apellido() { return this.contactForm.get('usuario.us_a'); }
-  get fnacimiento() { return this.contactForm.get('usuario.us_fn'); }
-  get tdocumento() { return this.contactForm.get('usuario.us_td'); }
-  get ndocumento() { return this.contactForm.get('usuario.us_nd'); }
-  get email() { return this.contactForm.get('usuario.us_c'); }
+  get nombre() { return this.contactForm.get('nombre'); }
+  get apellido() { return this.contactForm.get('apellido'); }
+  get fnacimiento() { return this.contactForm.get('fnacimiento'); }
+  get tdocumento() { return this.contactForm.get('tdocumento'); }
+  get ndocumento() { return this.contactForm.get('ndocumento'); }
+  get email() { return this.contactForm.get('email'); }
   
   ngOnInit() {
   }
@@ -85,6 +85,12 @@ export class PerfilmodalPage implements OnInit {
       this.datos = await this.promesaVerUsuario(token);
       this.datos = this.datos.result
       this.datos = this.datos.resultado
+      this.nombre.setValue(this.datos[0].us_n)
+      this.apellido.setValue(this.datos[0].us_a)
+      this.fnacimiento.setValue(this.datos[0].us_fn)
+      this.tdocumento.setValue(this.datos[0].us_td)
+      this.ndocumento.setValue(this.datos[0].us_nd)
+      this.email.setValue(this.datos[0].us_c)
     } catch (error) {
       console.log(error);
     }
@@ -93,19 +99,42 @@ export class PerfilmodalPage implements OnInit {
   private async editarDatos() {
     let result: any = null;
     let token: string = null;
+    let validar: any
+    const usuario = {
+      us_n:this.contactForm.value['nombre'],
+      us_a:this.contactForm.value['apellido'],
+      us_fn:this.contactForm.value['fnacimiento'],
+      us_td:this.contactForm.value['tdocumento'],
+      us_nd:this.contactForm.value['ndocumento'],
+      us_c:this.contactForm.value['email']
+    }
+
     try {
       token = await this.storage.get('token') || null
-      if(this.datos[0].us_n !== '' && this.datos[0].us_a !== '' && this.datos[0].us_nd !== null && this.datos[0].us_c !== '') {
-        result = await this.promesaActualizarUsuario(this.datos[0], token);
+      validar = this.validarDatos()
+      if(validar) {
+        result = await this.promesaActualizarUsuario(usuario, token);
         this.editar();
         this.modalCtrl.dismiss();
-      } else {
-        this.validarCampos();
       }
-
     } catch (error) {
       console.log(error);
     }
+  }
+
+  private validarDatos() {
+    let validar: boolean = true
+    if(
+      this.contactForm.value['nombre'] == this.datos[0].us_n && 
+      this.contactForm.value['apellido'] == this.datos[0].us_a && 
+      this.contactForm.value['fnacimiento'] == this.datos[0].us_fn && 
+      this.contactForm.value['tdocumento'] == this.datos[0].us_td && 
+      this.contactForm.value['ndocumento'] == this.datos[0].us_nd && 
+      this.contactForm.value['email'] == this.datos[0].us_c
+    ) {
+      validar = false
+    }
+    return validar
   }
 
   async validarCampos() {
